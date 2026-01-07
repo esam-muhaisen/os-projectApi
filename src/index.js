c0cd723a9fc5129076b59dfb24d5aa9ebfbf6ffb
esam muhaisen <109042732+esam-muhaisen@users.noreply.github.com>
2026-01-07 04:14:44 -0800

    update

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
     console.log(`server running in ${process.env.NODE_ENV}`);
 });
